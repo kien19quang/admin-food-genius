d4dc53d0734062ab5593edd593a951a24f10292a
kien19quang <kien19quang@gmail.com>
2024-02-20 15:23:49 +0700

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -181,14 +181,14 @@ export default Home
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
-  // if (!session) {
-  //   return {
-  //     redirect: {
-  //       destination: '/auth/signin',
-  //       permanent: false,
-  //     }
-  //   }
-  // }
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth/signin',
+        permanent: false,
+      }
+    }
+  }
 
   return {
     props: {}
